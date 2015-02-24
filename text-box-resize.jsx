8624c4bac94c0930b24ccd35b46fa3251b9254c2
Jordan Green <jagman1x3@gmail.com>
2015-02-23 23:05:04 -0500

    Resize all boxes
    Resizes all text boxes in the document, doesn't work with rotated boxes.

---
+++ text-box-resize.jsx
@@ -0,0 +1,16 @@
+﻿var PADDING = 10;
+var layers = app.activeDocument.artLayers;
+for (var i = 0; i < layers.length; i++){
+    var layer = layers[i];
+        if (layer.kind == LayerKind.TEXT){
+            if (layer.textItem.kind == TextType.PARAGRAPHTEXT){
+                var newtext = layer.duplicate();
+                newtext.rasterize(RasterizeType.ENTIRELAYER);
+                var w = newtext.bounds[2] - newtext.bounds[0], h = newtext.bounds[3] - newtext.bounds[1];
+                newtext.remove();
+                var text = layer.textItem;
+                text.width = w + PADDING;
+                text.height = h + PADDING;
+        }
+    }
+}
